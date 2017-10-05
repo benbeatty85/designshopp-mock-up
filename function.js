137b03b6bfa53ce7b2101de56fdf466ed9983b33
benbeatty85 <benbeatty85@gmail.com>
2017-10-05 14:31:04 -0400

    ready to add parallax effect

--- function.js
+++ function.js
@@ -1,4 +1,6 @@
- $(document).ready(function(){
+ //Navbar changes color when scrolling
+
+$(document).ready(function(){
       $(window).scroll(function() { // check if scroll event happened
         if ($(document).scrollTop() > 300) { // check if user scrolled more than 300 from top of the browser window
           $(".navbar-fixed-top").css("background-color", "#fff"); // if yes, then change the color of class "navbar-fixed-top" to white (#fff)
@@ -7,5 +9,18 @@
           $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
           $(".nav").css("color","#fff"); // if not, keep text color to white
         }
-      });
-    });
+        
+});
+});
+
+
+
+
+
+          
+
+
+
+
+
+
