17da5324692c2273252717f9605ecde11a85aae1
benbeatty85 <benbeatty85@gmail.com>
2017-10-05 19:12:59 -0400

    added pics and photo animation is in progress

--- function.js
+++ function.js
@@ -9,6 +9,70 @@ $(document).ready(function(){
           $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
           $(".nav").css("color","#fff"); // if not, keep text color to white
         }
+    
+  // Image parallax
+
+    var wScroll = $(this).scrollTop();
+          
+    if(wScroll > $('.social').offset().top - ($(window).height() / 1.5)) {
+
+      $('.one').each(function(){
+
+        $('.one').addClass('animate-one');
+
+      });
+
+    }
+          
+ if(wScroll > $('.social').offset().top - ($(window).height() / 1.5)) {
+
+      $('.two').each(function(){
+
+        $('.two').addClass('animate-two');
+
+      });
+
+    }
+          
+ if(wScroll > $('.social').offset().top - ($(window).height() / 1.2)) {
+
+      $('.three').each(function(){
+
+        $('.three').addClass('animate-three');
+
+      });
+
+    }
+          
+if(wScroll > $('.social').offset().top - ($(window).height() / 1.2)) {
+
+      $('.four').each(function(){
+
+        $('.four').addClass('animate-four');
+
+      });
+
+    }
+    
+if(wScroll > $('.social').offset().top - ($(window).height() / 2.0)) {
+
+      $('.five').each(function(){
+
+        $('.five').addClass('animate-five');
+
+      });
+
+    }
+          
+if(wScroll > $('.social').offset().top - ($(window).height() / 2.0)) {
+
+      $('.six').each(function(){
+
+        $('.six').addClass('animate-six');
+
+      });
+
+    }
         
 });
 });
