2a4af027d1c725c0f068abb7ab8f8bc43e13308e
benbeatty85 <benbeatty85@gmail.com>
2017-10-05 12:22:55 -0400

    navbar now changes color when scrolling past 300 px

---
+++ function.js
@@ -0,0 +1,11 @@
+ $(document).ready(function(){
+      $(window).scroll(function() { // check if scroll event happened
+        if ($(document).scrollTop() > 300) { // check if user scrolled more than 300 from top of the browser window
+          $(".navbar-fixed-top").css("background-color", "#fff"); // if yes, then change the color of class "navbar-fixed-top" to white (#fff)
+          $(".nav").css("color", "black"); //Change color of text so that it is visible when scrolling
+        } else {
+          $(".navbar-fixed-top").css("background-color", "transparent"); // if not, change it back to transparent
+          $(".nav").css("color","#fff"); // if not, keep text color to white
+        }
+      });
+    });
